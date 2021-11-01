a579cd9756dd337600007f2ef2232a473d5d02e3
Zhanibek <zhanibekkulmanov@gmail.com>
2021-11-01 18:40:59 +0600

    main: Sign In , Sign Up

---
+++ lib/presentation/auth/pages/sign_in.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class SingIn extends StatefulWidget {
+  const SingIn({Key? key}) : super(key: key);
+
+  @override
+  _SingInState createState() => _SingInState();
+}
+
+class _SingInState extends State<SingIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/presentation/auth/pages/sing_up.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class SingUp extends StatefulWidget {
+  const SingUp({Key? key}) : super(key: key);
+
+  @override
+  _SingUpState createState() => _SingUpState();
+}
+
+class _SingUpState extends State<SingUp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
