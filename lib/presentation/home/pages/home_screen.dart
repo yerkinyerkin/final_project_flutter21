6c00600b80654472d50295a64f565c24132d9622
Zhanibek <zhanibekkulmanov@gmail.com>
2021-11-02 14:47:02 +0600

    main: Sign In , Sign Up

--- lib/presentation/home/pages/home_screen.dart
+++ lib/presentation/home/pages/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,11 +12,84 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: SafeArea(
-        child: Container(
-          child: Text('Home', style: TextStyle(color: Colors.black),),
-          color: Colors.white,
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text(
+          'Home', style: TextStyle(fontSize: 30, fontWeight: FontWeight.w400),),
+      ),
+      body: Container(
+        color: Colors.black.withOpacity(0.07),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.add_circle_outline, size: 32,
+                        color: Color(0xff808A9D),),
+                      SizedBox(width: 4,),
+                      Text('Create Event', style: TextStyle(fontSize: 24,
+                          fontWeight: FontWeight.w400,
+                          color: Color(0xff808A9D)),)
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20,),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('SDU Clubs', style: TextStyle(
+                          fontWeight: FontWeight.w700, fontSize: 30),),
+                      CupertinoButton(
+                          padding: EdgeInsets.zero,
+                          onPressed: () {},
+                          child: Text('See all', style: TextStyle(
+                              fontWeight: FontWeight.w700,
+                              fontSize: 16,
+                              color: Color(0xff3861FB)),))
+                    ],
+                  ),
+                ),
+                SizedBox(height: 12,),
+                Container(
+                  height: 110,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: List.generate(10, (int index) {
+                      return Row(
+                        children: [
+                          SizedBox(width: 16,),
+                          Container(
+                              height: 110,
+                              width: 110,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.all(Radius.circular(10)),
+                                color: Colors.white,
+                              ),
+                            child: Column(
+                              children: [
+                                SvgPicture.asset('assets/icons/clublogo1.svg')
+                              ],
+                            ),
+                          ),
+                          index == 9 ? SizedBox(width: 16,) : Offstage()
+                        ],
+                      );
+                    }
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
