0e700d7efd55f58809db66c5d901e02a81a757dc
Berik <55269444+180107099@users.noreply.github.com>
2021-11-17 12:34:07 +0600

    Update EventCreate.dart

--- lib/presentation/home/pages/EventCreate.dart
+++ lib/presentation/home/pages/EventCreate.dart
@@ -1,156 +1,173 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:project/core/routes/routes_const.dart';
 
-class EventPage extends StatefulWidget {
-  const EventPage({Key? key}) : super(key: key);
+class EventCreate extends StatefulWidget {
+  const EventCreate({Key? key}) : super(key: key);
 
   @override
-  _EventPageState createState() => _EventPageState();
+  _EventCreateState createState() => _EventCreateState();
 
 }
 
-class _EventPageState extends State<EventPage> {
+class _EventCreateState extends State<EventCreate> {
 
+  DateTime _dateTime = DateTime.now();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          elevation: 0.0,
-          title: Text('Event', style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold, color: Colors.black)),
-          centerTitle: true,
-          leading: TextButton(
-            onPressed: () { Navigator.pushNamed(context, CreateEvent);},
-            child: Text('back', style: TextStyle(color: Color(0xff808A9D)),),
+     // backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text('Event Creating'),
+        backgroundColor: Colors.black,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.cancel_outlined,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              // do something
+            },
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child:Column(
+          children: <Widget>[
+            SizedBox(
+              height: 200,
+
+              child: CupertinoDatePicker(
+                backgroundColor: Color(0x000000),
+                initialDateTime: _dateTime,
+                onDateTimeChanged: (dateTime){
+                  print(dateTime);
+                  setState(() {
+                    _dateTime = dateTime;
+                  });
+                } ,
+                )
+              ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 15.0, right: 15.0, top: 15, bottom: 0),
+              //padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Title*',
+                    hintText: 'Title'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 15.0, right: 15.0, top: 20, bottom: 10,),
+              //padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Description*',
+                    hintText: 'Description'),
+              ),
+            ),
+            Container(
+              height: 100,
+              width: 250,
+              padding: const EdgeInsets.only(
+                  left: 15.0, right: 15.0, top: 20, bottom: 0),
+              decoration: BoxDecoration(
+                  color: Colors.blue, borderRadius: BorderRadius.circular(20)),
+              child: FlatButton(
+                onPressed: (){},
+                child: Text(
+                  'Upload image',
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 15.0, right: 15.0, top: 20, bottom: 10),
+              //padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Person limit',
+              ),
+              )),
+        Container(
+          padding: const EdgeInsets.only(
+              left: 15.0, right: 15.0, top: 20, bottom: 10),
+          child: Row(
+
+
+            children: <Widget>[
+
+              Expanded(
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text("Paid participation",),
+                ),
+              ),
+              Expanded(
+                child: RaisedButton(
+                  child: Text("Free"),
+                  color: Colors.red,
+
+                  onPressed: () {},
+                ),
+              )
+            ],
           ),
         ),
-        body: SingleChildScrollView(
-
-            child: SafeArea(
-                child: Column(
-                    children:<Widget>[
-                      Container(
-                          margin: EdgeInsets.only(top: 10, right: 10, left: 10),
-                          child: Column(
-                            children: <Widget>[
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: <Widget>[
-                                  Row(
-                                    children: <Widget>[
-                                      Container(
-                                        height: 55,
-                                        width: 55,
-                                        child: ClipRRect(
-                                          borderRadius: BorderRadius.all(Radius.circular(25)),
-                                          child: Image.asset("assets/profile_default.png"),
-                                        ),
-                                      ),
-                                      SizedBox(
-                                        width: 10,
-                                      ),
-                                      Row(
-                                        crossAxisAlignment: CrossAxisAlignment.start,
-                                        children: <Widget>[
-                                          Text(
-                                            "ALexsandrAlex",
-                                            style: TextStyle(
-                                                fontSize: 18, fontWeight: FontWeight.w500),
-                                          ),
-                                          SizedBox(
-                                            height: 5,
-                                          ),
-
-                                        ],
-                                      ),
-                                    ],
-                                  ),
-                                ],
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(left: 60, right: 10),
-                                child: Divider(
-                                  thickness: 1.50,
-                                ),
-                              ),
-                            ],
-                          )),
-
-
-                      CircleAvatar(
-                        radius: 127.5,
-                        backgroundColor: Colors.black,
-                        child: CircleAvatar(
-                          backgroundColor: Colors.white,
-                          child: SvgPicture.asset(
-                            '/profile_page_icons/profile_photo_placeholder.svg',
-                            width: 143,
-                            height: 186,
-                          ),
-                          radius: 126.5,
-                        ),
-                      ),
-                      Padding(
-                          padding: const EdgeInsets.only(
-                              left: 0, right: 15.0, top: 20, bottom: 10),
-                          //padding: EdgeInsets.symmetric(horizontal: 15),
-                          child: Text(
-
-                              'ds aasdg ag ag adg adf adrga dv dgf av agadv advag dv '
-                          )),
-
-                      Padding(
-                          padding: const EdgeInsets.only(
-                              left: 15.0, right: 0, top: 20, bottom: 10),
-                          //padding: EdgeInsets.symmetric(horizontal: 15),
-                          child: Text(
-                            'Requisits',
-                            style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold),
-
-                          )
-
-                      ),
-                      Padding(
-                          padding: const EdgeInsets.only(
-                              left: 15.0, right: 0, top: 20, bottom: 10),
-                          //padding: EdgeInsets.symmetric(horizontal: 15),
-                          child: Text(
-
-                            '8747 32 46 ',
-                            style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold),
-                          )),
-                      Padding(
-                          padding: const EdgeInsets.only(
-                              left: 15.0, right: 0, top: 20, bottom: 10),
-                          //padding: EdgeInsets.symmetric(horizontal: 15),
-                          child: Text(
-
-                            'xxx xxx xxxxx xxx',
-                            style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold),
-                          )),
-                      Container(
-                        height: 50,
-                        width: 250,
-                        padding: const EdgeInsets.only(
-                            left: 15.0, right: 15.0, top: 20, bottom: 0),
-                        decoration: BoxDecoration(
-                            color: Colors.blue, borderRadius: BorderRadius.circular(20)),
-                        child: FlatButton(
-                          onPressed: (){},
-                          child: Text(
-                            'Participate',
-                            style: TextStyle(color: Colors.white, fontSize: 22),
-                          ),
-                        ),
-                      ),
-
-                    ]
-                )
-            )));
+            Padding(
+                padding: const EdgeInsets.only(
+                    left: 15.0, right: 15.0, top: 20, bottom: 10),
+                //padding: EdgeInsets.symmetric(horizontal: 15),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Price',
+                  ),
+                )),
+            Padding(
+                padding: const EdgeInsets.only(
+                    left: 15.0, right: 15.0, top: 20, bottom: 10),
+                //padding: EdgeInsets.symmetric(horizontal: 15),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Credit card number',
+                  ),
+                )),
+            Padding(
+                padding: const EdgeInsets.only(
+                    left: 15.0, right: 15.0, top: 20, bottom: 10),
+                //padding: EdgeInsets.symmetric(horizontal: 15),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Phone number',
+                  ),
+                )),
+            Container(
+              height: 100,
+              width: 250,
+              padding: const EdgeInsets.only(
+                  left: 15.0, right: 15.0, top: 20, bottom: 0),
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(20)),
+              child: FlatButton(
+                onPressed: (){},
+                child: Text(
+                  'Save',
+                  style: TextStyle(color: Colors.black, fontSize: 25),
+                ),
+              ),
+            ),
+          ],
+        ),
+      )
+    );
   }
 }
-
