a0961a9e079a1b748f08b02949cdd410597e2b1a
qaratay <galymzhanqaratay@gmail.com>
2021-11-17 15:17:59 +0600

    edit info

--- lib/core/routes/router.dart
+++ lib/core/routes/router.dart
@@ -4,12 +4,10 @@ import 'package:project/core/routes/routes_const.dart';
 import 'package:project/core/widgets/buttons/back_button.dart';
 import 'package:project/presentation/home/pages/EventCreate.dart';
 import 'package:project/presentation/main/main_screen.dart';
-
+import 'package:project/presentation/profile/pages/edit_info.dart';
 
 class InnLabRouter {
-
   static Route<dynamic> generateRoute(RouteSettings routeSettings) {
-
     final parts = routeSettings.name!.split('?');
 
     switch (parts[0]) {
@@ -23,24 +21,28 @@ class InnLabRouter {
           settings: routeSettings,
           builder: (_) => EventCreate(),
         );
+      case MainInfoEdit:
+        return CupertinoPageRoute(
+          settings: routeSettings,
+          builder: (_) => EditInfo(),
+        );
       default:
         return CupertinoPageRoute(
           settings: routeSettings,
-          builder: (_) =>
-              CupertinoPageScaffold(
-                navigationBar: CupertinoNavigationBar(
-                  leading: InnLabNavigationBackButton(
-                    color: Colors.black,
-                  ),
-                  middle: Text('Навигация'),
-                ),
-                child: Center(
-                  child: Text(
-                    'Ошибка, роут для ${routeSettings.name} не найден',
-                    textAlign: TextAlign.center,
-                  ),
-                ),
+          builder: (_) => CupertinoPageScaffold(
+            navigationBar: CupertinoNavigationBar(
+              leading: InnLabNavigationBackButton(
+                color: Colors.black,
               ),
+              middle: Text('Навигация'),
+            ),
+            child: Center(
+              child: Text(
+                'Ошибка, роут для ${routeSettings.name} не найден',
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
         );
     }
   }
--- lib/core/routes/routes_const.dart
+++ lib/core/routes/routes_const.dart
@@ -1,7 +1,6 @@
-
-
 const String MainRoute = '/main';
 const String LoginRoute = '/login';
 
 const String CreateEvent = '/eventCreate';
 
+const String MainInfoEdit = '/edit_info';
